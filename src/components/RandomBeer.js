44cd8a5066c5e0ef73feccdadee9e1a04f72c6a6
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-11-07 22:45:29 +0100

    add iteration 5 (not refactored)

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,11 +1,38 @@
 import React from "react";
-import Header from "./Header"
+import axios from "axios";
+import Header from "./Header";
 
 class RandomBeer extends React.Component {
+
+    state= {
+        randomBeer: {}
+    }
+
+    getBeer = () => {
+        axios.get("https://ih-beers-api2.herokuapp.com/beers/random")
+        .then( response =>{
+            console.log('random response', response.data)
+            this.setState({randomBeer: response.data});
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+    componentDidMount(){    
+        this.getBeer()
+    }
+ 
     render() {
         return (
             <>
-                 <Header/>  
+                <Header/>
+                <h3>{this.state.randomBeer.name}</h3>
+                <img src={this.state.randomBeer.image_url} alt="image" style={{ width: "50px" }} />
+                <p>{this.state.randomBeer.tagline}</p>
+                <p>{this.state.randomBeer.attenuation_level}</p> 
+                <p>{this.state.randomBeer.description}</p> 
+                <p>{this.state.randomBeer.contributed_by}</p> 
+
             </>
         )
     }
